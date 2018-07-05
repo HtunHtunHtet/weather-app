ec6c60f9be1c78c036f2ad12815cddf9ebdf4d7d
htunhtunhtet <ryanhhh91@gmail.com>
2018-07-05 15:18:31 +0800

    bug asking in facebook

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,24 +1,13 @@
 import React from 'react';
+import ZipCode from './ZipCode';
+
 class Home extends React.Component{
     render(){
         return(
             <div className="home-container" style={{backgroundImage: "url('app/images/pattern.svg')"}}>
                 <h1 className="header">Enter a City and State</h1>
                 <div className="zip-container"  style={{flexDirection: this.props.direction}}>
-                    <form className="main-form">
-                        <div>
-                            <input
-                                className="form-control"
-                                type="text"
-                                placeholder="Malaysia"
-                            />
-                        </div>
-                        <div style={{textAlign:'center' , marginBottom: 10, marginTop: 10}}>
-                            <button className="btn btn-success">
-                                Get Weather
-                            </button>
-                        </div>
-                    </form>
+                        <ZipCode/>
                 </div>
             </div>
         )

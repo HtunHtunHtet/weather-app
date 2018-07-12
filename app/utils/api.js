5641dcc398799b8a2ab8e0c69789e9eabec71d53
htunhtunhtet <ryanhhh91@gmail.com>
2018-07-12 18:22:14 +0800

    Finishing up

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const apiKey    = 'b714ec74bbab5650795063cb0fdf5fbe';
-const baseURL   =  'http://api.openweathermap.org/data/2.5/';
+const baseURL   =  'https://api.openweathermap.org/data/2.5/';
 
 function getCurrentWeather(cityName){
     return axios.get(`${baseURL}weather?q=${cityName}&type=accurate&APPID=${apiKey}`)
@@ -10,7 +10,7 @@ function getCurrentWeather(cityName){
 }
 
 function getFiveDayForecast(cityName) {
-    return axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&type=accurate&APPID=${apiKey}&cnt=5`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&type=accurate&APPID=${apiKey}&cnt=5`)
         .then((response) => (response.data))
         .catch((error) => (error));
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var path=require('path');
 var HtmlWebpackPlugin = require ('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports= {
+//NOD_ENV variable for production
+//Uglify or minify
+
+var config= {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -21,4 +25,17 @@ module.exports= {
         new HtmlWebpackPlugin({
             template: 'app/index.html'
     })]
+};
+
+if(process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env' : {
+                'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    )
 }
+
+
+module.exports = config;
